774904fba03f2e22dcfdb7d3a8c014efd6ca008e
abel-hak <hakensoabel@gmail.com>
2024-11-10 13:34:57 +0300

    Update models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Author(models.Model):
     
     name = models.CharField(max_length=225)
+    def __str__(self):
+        return self.name
     
 class Book(models.Model):
     
