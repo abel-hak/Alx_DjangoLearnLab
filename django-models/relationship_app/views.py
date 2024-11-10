c723e317c62194a91fa24abbc6ed9faea78103c1
abel-hak <hakensoabel@gmail.com>
2024-11-10 16:17:40 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
-from .models import Book, Library
-from .models import Library
+from .models import Book, Library, UserProfile
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.views import View
+from django.contrib.auth.decorators import user_passes_test
 
+
+# Register view
 class RegisterView(View):
     def get(self, request):
         form = UserCreationForm()
@@ -30,6 +32,7 @@ def list_books(request):
     }
     return render(request, 'relationship_app/list_books.html', context)
 
+
 # Class-based view to display details for a specific library with all its books
 class LibraryDetailView(DetailView):
     model = Library
@@ -40,3 +43,33 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['books'] = self.object.books.all()  # Get all books related to the library
         return context
+
+
+# Role-based access views
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+
+# Admin view - accessible only to 'Admin' users
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+
+# Librarian view - accessible only to 'Librarian' users
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+
+# Member view - accessible only to 'Member' users
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
