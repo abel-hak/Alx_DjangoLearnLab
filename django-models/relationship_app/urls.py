f0948d114d34c3fe119b646690af1386b44c0892
abel-hak <hakensoabel@gmail.com>
2024-11-10 16:21:22 +0300

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -12,6 +12,10 @@ urlpatterns = [
     
     # Logout view with a specified template
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
     
     path('books/', views.list_books, name='list_books'),  # Function-based view for listing books
     
