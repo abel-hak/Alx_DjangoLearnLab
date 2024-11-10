1ff2c207b1ffa2c437d4671e4a956d12e420d67d
abel-hak <hakensoabel@gmail.com>
2024-11-10 16:27:15 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -60,16 +60,16 @@ def is_member(user):
 # Admin view - accessible only to 'Admin' users
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'relationship_app/admin_view.html')
 
 
 # Librarian view - accessible only to 'Librarian' users
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
 
 # Member view - accessible only to 'Member' users
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'member_view.html')
+    return render(request, 'relationship_app/member_view.html')
