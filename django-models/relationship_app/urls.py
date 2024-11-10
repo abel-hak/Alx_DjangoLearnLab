71d16ded1befb00bb1bba139c522ad6dc84a2686
abel-hak <hakensoabel@gmail.com>
2024-11-10 15:57:58 +0300

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books
 
 urlpatterns = [
     # Registration view
-    path('register/', views.RegisterView.as_view(), name='register'),
+    path('register/', views.register, name='register'),
     
     # Login view
-    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     
     # Logout view with a specified template
-    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     
     path('books/', views.list_books, name='list_books'),  # Function-based view for listing books
     
