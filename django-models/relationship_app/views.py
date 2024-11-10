ad75535ae3039154481ecbee10b52a2929dee034
abel-hak <hakensoabel@gmail.com>
2024-11-10 15:27:14 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,25 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
 from .models import Book, Library
 from .models import Library
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+from django.views import View
+
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'relationship_app/register.html', {'form': form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log the user in after registration
+            return redirect('home')  # Redirect to the home page or another page after registration
+        return render(request, 'relationship_app/register.html', {'form': form})
+
 
 # Function-based view to list all books with their authors
 def list_books(request):
