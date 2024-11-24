f7bb4639227ed4b3cc2aa88bf06b44f8eaa2abbe
Abel <hakensoabel@gmail.com>
2024-11-24 19:19:54 +0300

    Added API endpoint for listing books

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import viewsets
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing and editing Book instances.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
