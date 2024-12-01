0b612cc4c77185ae0d66e74b0973d31454f5c915
Abel <hakensoabel@gmail.com>
2024-12-01 18:24:36 +0300

    Initial commit: Advanced API Project with custom serializers

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,7 +3,7 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 class BookListView(ListAPIView):
