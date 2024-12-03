2ec124657db9b84a84ac83a1b8cbf6657ea95e60
Abel <hakensoabel@gmail.com>
2024-12-03 17:51:04 +0300

    Initial commit: Set up Django blog project structure

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,4 +18,7 @@ urlpatterns = [
     path('post/<int:post_pk>/comments/new/', views.CommentCreateView.as_view(), name='add_comment'),
     path('post/<int:post_pk>/comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='edit_comment'),
     path('post/<int:post_pk>/comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete_comment'),
+     path('posts/<int:post_pk>/comments/new/', views.CommentCreateView.as_view(), name='add_comment'),
+    path('comments/<int:pk>/update/', views.CommentUpdateView.as_view(), name='edit_comment'),
+    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete_comment'),
 ]
