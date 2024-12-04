8e00b534062344c200846a3295f1b9b0e05a2b79
Abel <hakensoabel@gmail.com>
2024-12-04 17:17:46 +0300

    Initial commit: Set up Django blog project structure

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     
     path('tags/<slug:tag_slug>/', views.posts_by_tag, name='posts_by_tag'),
     path('search/', views.search_posts, name='search_posts'),
+     path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='posts_by_tag'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -190,3 +190,27 @@ def posts_by_tag(request, tag_slug):
     return render(request, 'blog/posts_by_tag.html', {'tag': tag, 'posts': posts})
 
 
+from django.views.generic.list import ListView
+from .models import Post
+from django.shortcuts import get_object_or_404
+from taggit.models import Tag
+
+
+class PostByTagListView(ListView):
+    model = Post
+    template_name = 'blog/posts_by_tag.html'  # Create this template
+    context_object_name = 'posts'
+    paginate_by = 10  # Optional: pagination
+
+    def get_queryset(self):
+        tag_slug = self.kwargs.get('tag_slug')
+        self.tag = get_object_or_404(Tag, slug=tag_slug)
+        return Post.objects.filter(tags__in=[self.tag]).distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag'] = self.tag
+        return context
+
+
+
