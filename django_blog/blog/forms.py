267f2fd514b62905e394b96b47db4bec1f5194e8
Abel <hakensoabel@gmail.com>
2024-12-04 17:14:45 +0300

    Initial commit: Set up Django blog project structure

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,13 +1,16 @@
 from django import forms
-from .models import Post
-from .models import Comment
-from taggit.forms import TagField
+from .models import Post, Comment
+from taggit.forms import TagField, TagWidget
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']  # Include tags
+        widgets = {
+            'tags': TagWidget(attrs={'placeholder': 'Add tags separated by commas'}),
+        }
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
