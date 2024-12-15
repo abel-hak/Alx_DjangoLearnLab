ba15e5a494b642ba81b6fb66829cfebfbe315cbd
Abel <hakensoabel@gmail.com>
2024-12-15 13:57:56 +0300

    Normalize line endings

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,14 +1,51 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    token = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture', 'token']
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        # Create a new user
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None),
+        )
+        # Generate a token for the user
+        Token.objects.create(user=user)
         return user
+
+    def get_token(self, obj):
+        # Retrieve the token for the user
+        token, _ = Token.objects.get_or_create(user=obj)
+        return token.key
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    token = serializers.SerializerMethodField()
+
+    def validate(self, data):
+        from django.contrib.auth import authenticate
+        user = authenticate(username=data['username'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError("Invalid username or password.")
+        self.user = user
+        return data
+
+    def get_token(self, obj):
+        # Retrieve or create a token for the user
+        token, _ = Token.objects.get_or_create(user=self.user)
+        return token.key
+
